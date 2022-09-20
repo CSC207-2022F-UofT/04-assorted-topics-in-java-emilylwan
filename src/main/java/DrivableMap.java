3e6204296618711b33acbb2be497180c22742030
emilylwan <93227982+emilylwan@users.noreply.github.com>
2022-09-20 10:59:08 -0400

    Finished

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -22,32 +22,46 @@ class DrivableMap {
         drivable_map = new HashMap<>();
     }
 
-    /* TODO: Write a method named addDrivable that takes a String (the ID)
+    /* Write a method named addDrivable that takes a String (the ID)
      *       and a Drivable object. If the ID string does not appear as a key
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
+    public Boolean addDrivable(String id, Drivable item) {
+        if(!drivable_map.containsKey(id)) {
+            drivable_map.put(id, item);
+            return true;
+        }
+        return false;
+    }
 
-
-
-
-    /* TODO: Write a method named hasFasterThan that takes an int (a speed)
+    /* Write a method named hasFasterThan that takes an int (a speed)
      *       and returns true iff there is at least one item in drivable_map
      *       that has a maxSpeed >= the speed given.
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
-
-
-
+    public Boolean hasFasterThan(int speed) {
+        for(Drivable i: drivable_map.values()) {
+            if(i.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List<Tradable> getTradable() {
+        List<Tradable> tradableItems= new ArrayList<Tradable>();
+        for(Drivable i: drivable_map.values()) {
+            if(i instanceof Tradable) {
+                tradableItems.add((Tradable) i);
+            }
+        }
+        return tradableItems;
+    }
 
-
-
-    
 }
--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -35,34 +35,36 @@ public class Trader<T> {
         this.money = money;
     }
 
-    /* TODO: Add a new constructor that takes a single argument
+    /* Add a new constructor that takes a single argument
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
+    public Trader(int money) {
+        this.inventory = new ArrayList<T>();
+        this.wishlist = new ArrayList<T>();
+        this.money = money;
+    }
 
-
-
-
-
-    /* TODO: Implement the method addToWishlist that takes an
+    /* Implement the method addToWishlist that takes an
      *       object of type T and adds it to this Trader's wishlist.
      */
+    public void addToWishlist(T item) {
+        this.wishlist.add(item);
+    }
 
-
-
-
-
-    /* TODO: Implement the method getSellingPrice that takes an
+    /* Implement the method getSellingPrice that takes an
      *       object of type T and returns the object's price
      *       (via getPrice()) if it's Tradable. If not,
      *       return Tradable.MISSING_PRICE.
      *
      *       We will call this in exchangeMoney().
      */
-
-
-
-
+    public int getSellingPrice(T item) {
+        if(item instanceof Tradable) {
+            return ((Tradable) item).getPrice();
+        }
+        return Tradable.MISSING_PRICE;
+    }
 
     /**
      * Exchange money from other to this Trader according to the price of item,
